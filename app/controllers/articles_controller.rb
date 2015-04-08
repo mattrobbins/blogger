3112180d64e70f7b83944a08db50f5c31a74dcc9
Matt Robbins <robbinsmk@gmail.com>
2015-04-08 12:00:33 -0600

    form-based workflow feature completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper
 
   def index
     @articles = Article.all
@@ -9,6 +10,54 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
+  end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def create
+
+    #This was the first way to add data through the form
+    #that was shown.
+    #@article = Article.new
+    #@article.title = params[:article][:title]
+    #@article.body = params[:article][:body]
+
+
+    #This is the second way passing params as a hash
+    #@article = Article.new(
+    #  title: params[:article][:title],
+    #  body: params[:article][:body]
+
+
+    #This can be used after the right code is added to the
+    #article_helper class.
+    @article = Article.new(article_params)
+
+
+    @article.save
+    flash.notice = "Article '#{@article.title}' was created!"
+
+    redirect_to article_path(@article)
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
+    flash.notice = "Article '#{@article.title}' Updated!"
+
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    flash.notice = "Article '#{@article.title}' was deleted!"
+
+    redirect_to action: "index"
   end
 end
